df65fcb2690f35a90e58e64d00ced0d329c739cd
Nahuel <lescanoagustin10nahuel@gmail.com>
2026-06-07 18:37:28 -0300

    refactor: los middlewares ya no estan aca

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,9 @@
-import express from "express";
-import morgan from "morgan";
 import { PORT, API_V1 } from "../config";
 import { pokemonRouter } from "./routes";
-
-const server = express();
-
-server.use(morgan("dev"));
-server.use(express.json());
+import { server } from "./middleware";
 
 server.use(`${API_V1}pokemons`, pokemonRouter);
 
 server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`API endpoint: http://localhost:${PORT}${API_V1}pokemons`);
 });
