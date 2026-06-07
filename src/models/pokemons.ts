3030742429554d605ef6428e083dc67732ec8a5f
Nahuel <lescanoagustin10nahuel@gmail.com>
2026-06-07 16:23:17 -0300

    feat: los modelos que tendran cada pokemon

--- src/models/pokemons.ts
+++ src/models/pokemons.ts
@@ -4,12 +4,4 @@ export interface Pokemon {
   type: string;
 }
 
-export const POKEMONS: Pokemon[] = [
-  { id: 1, name: "Bulbasaur", type: "Grass/Poison" },
-  { id: 2, name: "Charmander", type: "Fire" },
-  { id: 3, name: "Squirtle", type: "Water" },
-  { id: 4, name: "Pikachu", type: "Electric" },
-  { id: 5, name: "Eevee", type: "Normal" },
-] as const;
-
-export default POKEMONS;
+export default Pokemon;
