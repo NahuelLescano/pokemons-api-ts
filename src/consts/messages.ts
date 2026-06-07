d7195aaf7df434fb0d9252ea6e2e82aa2333c85e
Nahuel <lescanoagustin10nahuel@gmail.com>
2026-06-07 18:33:13 -0300

    fix: mejoro el nombre del mensaje

--- src/consts/messages.ts
+++ src/consts/messages.ts
@@ -2,7 +2,7 @@ export const messages = {
   SUCCESS: "Pokemons retrieved successfully",
   INVALID_ID: "Invalid ID format",
   NOT_FOUND: "Pokemon not found",
-  NAME_TYPE_REQUIRED: "Name and type are required",
+  INVALID_POKEMON: "Name and type are required",
   CREATED_SUCCESS: "Pokemon created successfully",
   UPDATED_SUCCESS: "Pokemon updated successfully",
   DELETED_SUCCESS: "Pokemon deleted successfully",
