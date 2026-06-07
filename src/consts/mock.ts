8b8acb64517f787e9661766151f403c586c07c90
Nahuel <lescanoagustin10nahuel@gmail.com>
2026-06-07 18:33:42 -0300

    fix: mejorando los tipados

--- src/consts/mock.ts
+++ src/consts/mock.ts
@@ -1,11 +1,12 @@
 import type { Pokemon } from "../models";
 
 export const POKEMONS: Pokemon[] = [
-  { id: 1, name: "Bulbasaur", type: "Grass/Poison" },
-  { id: 2, name: "Charmander", type: "Fire" },
-  { id: 3, name: "Squirtle", type: "Water" },
-  { id: 4, name: "Pikachu", type: "Electric" },
-  { id: 5, name: "Eevee", type: "Normal" },
+  { id: 1, name: "Bulbasaur", type: "Grass/Poison", rarity: "Common", deleted: false },
+  { id: 2, name: "Charmander", type: "Fire", rarity: "Common", deleted: false },
+  { id: 3, name: "Squirtle", type: "Water", rarity: "Common", deleted: false },
+  { id: 4, name: "Pikachu", type: "Electric", rarity: "Uncommon", deleted: false },
+  { id: 5, name: "Eevee", type: "Normal", rarity: "Uncommon", deleted: false },
+  { id: 6, name: "Mewtwo", type: "Psychic", rarity: "Legendary", deleted: false },
 ] as const;
 
 export default POKEMONS;
--- src/models/pokemons.ts
+++ src/models/pokemons.ts
@@ -1,7 +1,20 @@
+export type PokemonType = 
+  "Fire"         |
+  "Water"        |
+  "Grass"        |
+  "Electric"     |
+  "Psychic"      |
+  "Grass/Poison" |
+  "Normal";
+
+export type PokemonRarity = "Common" | "Uncommon" | "Rare" | "Legendary";
+
 export interface Pokemon {
   id: number;
   name: string;
-  type: string;
+  type: PokemonType;
+  rarity: PokemonRarity;
+  deleted: boolean;
 }
 
 export default Pokemon;
