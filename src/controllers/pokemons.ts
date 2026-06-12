35c6c585ac8cf34b7fe941e61da3b36c2ad44d1a
Nahuel <lescanoagustin10nahuel@gmail.com>
2026-06-11 23:52:24 -0300

    refactor: data es muy generico, devuelvo pokemones

--- src/controllers/pokemons.ts
+++ src/controllers/pokemons.ts
@@ -9,25 +9,30 @@ export const getAllPokemons = async (req: Request, res: Response) => {
   if (error) {
     res
       .status(HttpStatusCodes.BAD_REQUEST)
-      .json({ message: error.message, result: [] });
+      .json({ message: error.message, pokemon: [] });
     return;
   }
 
-  res.json({ message: messages.SUCCESS, result: result.data });
+  res.json({ message: messages.SUCCESS, pokemons: result.pokemons });
 };
 
 export const getPokemonById = async (req: Request, res: Response) => {
   const { id } = req.params;
-  const { result, error } = await tryCatch(getById(Number(id)));
+  const pokeId = Number(id);
+  if (isNaN(pokeId)) {
+    throw new Error(messages.INVALID_ID);
+  }
+
+  const { result, error } = await tryCatch(getById(pokeId));
 
   if (error) {
     res
       .status(HttpStatusCodes.BAD_REQUEST)
-      .json({ message: error.message, result: [] });
+      .json({ message: error.message, pokemon: [] });
     return;
   }
 
-  res.json({ message: messages.SUCCESS, result });
+  res.json({ message: messages.SUCCESS, pokemon: result.pokemon });
 };
 
 export const createPokemon = async (req: Request, res: Response) => {
@@ -37,71 +42,87 @@ export const createPokemon = async (req: Request, res: Response) => {
   if (error) {
     res
       .status(HttpStatusCodes.BAD_REQUEST)
-      .json({ message: error.message, result: [] });
+      .json({ message: error.message, pokemon: [] });
     return;
   }
 
   res
     .status(HttpStatusCodes.CREATED)
-    .json({ message: messages.CREATED_SUCCESS, result });
+    .json({ message: messages.CREATED_SUCCESS, pokemon: result.pokemon });
 };
 
 export const updatePokemon = async (req: Request, res: Response) => {
   const { id } = req.params;
-  const { name, type, rarity } = req.body;
+  const pokeId = Number(id);
+  if (isNaN(pokeId)) {
+    throw new Error(messages.INVALID_ID);
+  }
 
-  const { result, error } = await tryCatch(
-    update(Number(id), name, type, rarity),
-  );
+  const { name, type, rarity } = req.body;
+  if (!name || !type || !rarity) {
+    throw new Error(messages.INVALID_POKEMON);
+  }
+
+  const { result, error } = await tryCatch(update(pokeId, name, type, rarity));
 
   if (error) {
     res
       .status(HttpStatusCodes.BAD_REQUEST)
-      .json({ message: error.message, result: [] });
+      .json({ message: error.message, pokemon: [] });
     return;
   }
 
   res.json({
     message: messages.UPDATED_SUCCESS,
-    result,
+    pokemon: result.pokemon,
   });
 };
 
 export const patchPokemon = async (req: Request, res: Response) => {
   const { id } = req.params;
-  const { name, type, rarity } = req.body;
+  const pokeId = Number(id);
+  if (isNaN(pokeId)) {
+    throw new Error(messages.INVALID_ID);
+  }
 
-  const { result, error } = await tryCatch(
-    patch(Number(id), name, type, rarity),
-  );
+  const { name, type, rarity } = req.body;
+  if (!name && !type) {
+    throw new Error(messages.PATCH_FIELD_REQUIRED);
+  }
+
+  const { result, error } = await tryCatch(patch(pokeId, name, type, rarity));
 
   if (error) {
     res
       .status(HttpStatusCodes.BAD_REQUEST)
-      .json({ message: error.message, result: [] });
+      .json({ message: error.message, pokemon: [] });
     return;
   }
 
   res.json({
     message: messages.UPDATED_SUCCESS,
-    result,
+    pokemon: result.pokemon,
   });
 };
 
 export const deletePokemon = async (req: Request, res: Response) => {
   const { id } = req.params;
+  const pokeId = Number(id);
+  if (isNaN(pokeId)) {
+    throw new Error(messages.INVALID_ID);
+  }
 
-  const { result, error } = await tryCatch(drop(Number(id)));
+  const { result, error } = await tryCatch(drop(pokeId));
 
   if (error) {
     res
       .status(HttpStatusCodes.BAD_REQUEST)
-      .json({ message: error.message, result: [] });
+      .json({ message: error.message, pokemon: [] });
     return;
   }
 
   res.json({
     message: messages.DELETED_SUCCESS,
-    result,
+    pokemon: result.pokemon,
   });
 };
