ffae4e12bd2a376ad5f842e2b13f45bf3d87bff8
Nahuel <lescanoagustin10nahuel@gmail.com>
2026-06-11 23:51:59 -0300

    feat: agrego mas mensajes

--- src/consts/messages.ts
+++ src/consts/messages.ts
@@ -4,7 +4,10 @@ export const messages = {
   NOT_FOUND: "Pokemon not found",
   INVALID_POKEMON: "Name and type are required",
   CREATED_SUCCESS: "Pokemon created successfully",
+  CREATE_FAILED: "Failed to create Pokemon",
   UPDATED_SUCCESS: "Pokemon updated successfully",
+  UPDATED_FAILED: "Failed to update Pokemon",
   DELETED_SUCCESS: "Pokemon deleted successfully",
+  DELETED_FAILED: "Failed to delete Pokemon",
   PATCH_FIELD_REQUIRED: "At least one field is required",
 };
