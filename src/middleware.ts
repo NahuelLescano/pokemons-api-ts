8139d306100120625b6fa4aa9ba862b360d56bfc
Nahuel <lescanoagustin10nahuel@gmail.com>
2026-06-20 13:37:04 -0300

    feat: uso directamente cors

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,10 @@
-import express, { type Request, type Response, type NextFunction } from "express";
+import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 
 export const server = express();
 
-server.use((_req: Request, res: Response, next: NextFunction) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
-
+server.use(cors());
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use(morgan("dev"));
